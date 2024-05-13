f6036c1afa106464f8ce292df7f0d289339b1ed8
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-05-14 00:47:27 +0300

    version: 1.0.1 / fix: display 100vh on mobile devices

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import './styles/style.css';
 import Slider from "./scripts/slider";
 
 Slider();
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
