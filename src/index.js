cf0eb89926fc62d7cc45192390e2a42232526177
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-05-14 01:04:26 +0300

    version: 1.0.2 / fix: add a viewport change handler

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import Slider from "./scripts/slider";
 
 Slider();
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
