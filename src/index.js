f0af5610ae55ec7f6cace7d358cab16357df3c3b
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-05-14 01:08:41 +0300

    fix: viewport

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './vendor/normalize.css';
 import './styles/style.css';
 import Slider from "./scripts/slider";
 
-Slider();
-
-window.addEventListener('resize', () => {
+function setViewport() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+Slider();
+setViewport();
+window.addEventListener('resize', setViewport());
