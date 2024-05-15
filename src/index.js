ef63da67901391b7199892220b8aaf09e835144a
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-05-15 18:40:09 +0300

    feat: add menu btn

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import './vendor/normalize.css';
 import './scss/index.scss';
 import Slider from "./scripts/slider";
 
+const menuButton = document.querySelector('.menu__button');
+const menu = document.querySelector('.menu__list');
+
 function setViewport() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
+
+function handleMenuVisible() {
+    menu.classList.toggle('menu__list_open');
+}
+//Menu
+
+menuButton.addEventListener('mousedown', handleMenuVisible);
 
 Slider();
 setViewport();
